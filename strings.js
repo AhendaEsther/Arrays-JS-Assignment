b2842d647f3fb6acf9cb5d3494c163f88792ee1b
esther ahenda <eahenda5@gamil.com>
2025-03-26 07:48:19 +0300

    initial commit

---
+++ strings.js
@@ -0,0 +1,37 @@
+
+
+
+//Finding the last element of an array
+let array1 =[3,7,34,90,12,15];
+let array1x= (array1.length-1);
+console.log(array1x);
+
+//Converting arrays to strings
+let myPets = ["cow","bird","snake","dog"]
+let myPets1 =myPets.toString();
+console.log(myPets1);
+
+//sorting arrays
+let array3 =[-5,9,5,3,2,-3,6,8,4,1];
+let array3x =array3.sort();
+console.log(array3x);
+
+//Remove duplicates
+let array = ["boy","man","girl","school","girl","woman"]
+let array4 = array.filter();
+console.log(array4);
+
+//Search for a word
+let arr5 = ["the","way","x","4"];
+let arr5x =arr5.search(food);
+console.log(arr5x);
+let arr6 = arr5.replace(food);
+console.log(arr6);
+
+let word = "renniw"
+let word1 =word.sort();
+console.log(word1);
+
+let fruits =["mango","oranges","banana","carrot","apple","watermelon","melon","peas","dates","ovacado",]
+let fruits1 = "Tomato".indexOf(4);
+console.log(fruits1);
